ac0d9b328baabe4e6acb3faa174e9266fa1f25d8
Analytical Learner <steven@analytciallearner.com>
2025-05-14 09:12:50 -0400

    Added comment in the slsynch.sh script.

--- slsync.sh
+++ slsync.sh
@@ -1,3 +1,5 @@
 # !/bin/bash
+# This script will sync your local repository to your remote respoistory
+# once you enter in the path to the two locations.
 
 sudo rsync -avh --delete ssh ~/stoneleaf/local/ user@[SERVER]:/path/to/library
